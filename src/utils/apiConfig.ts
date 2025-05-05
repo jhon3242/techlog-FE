1341cb9a7025c001b29e44a50c520099b670bc62
jhon3242 <rldotnr2@gmail.com>
2025-05-05 12:59:56 +0900

    feat: base url 변경

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,6 +1,6 @@
 // src/utils/apiConfig.ts
-// export const API_BASE_URL = 'https://techlog.p-e.kr';
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://techlog.p-e.kr';
+// export const API_BASE_URL = 'http://localhost:8080';
 
 // 로컬스토리지에서 관리자 헤더 값을 가져오는 함수
 export const getAdminHeaderFromStorage = (): string => {
